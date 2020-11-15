5ab5b6b91308eba2c8dd5a6f70b5b3679abd7db6
Stefan Radusi <stefan.radusi@aeratechnology.com>
2020-11-15 21:54:45 +0200

    make landing page

--- site/src/App.js
+++ site/src/App.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Auth from './pages/Auth/Auth'
-import Dashboard from './pages/Dashboard/Dashboard'
-import { getSession } from './utils'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// import Auth from "./pages/Auth/Auth";
+// import Dashboard from "./pages/Dashboard/Dashboard";
+import { Home } from "./pages/Home";
+// import { getSession } from "./utils";
 
 export default class App extends Component {
-
   constructor(props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = {};
   }
 
   async componentDidMount() {
@@ -24,35 +20,20 @@ export default class App extends Component {
     return (
       <Router>
         <Switch>
-
-          <Route path='/register'>
-            <Auth />
-          </Route>
-
-          <Route path='/login'>
-            <Auth />
-          </Route>
-
-          <PrivateRoute
-            exact
-            path='/'
-            component={Dashboard}
-          />
-
+          <Route component={Home} />
         </Switch>
       </Router>
-    )
+    );
   }
 }
 
 /**
  * A component to protect routes.
  * Shows Auth page if the user is not authenticated
- */
-const PrivateRoute = ({ component, ...options }) => {
+//  */
+// const PrivateRoute = ({ component, ...options }) => {
+//   const session = getSession();
 
-  const session = getSession()
-
-  const finalComponent = session ? Dashboard : Home
-  return <Route {...options} component={finalComponent} />
-}
+//   const finalComponent = session ? Dashboard : Home;
+//   return <Route {...options} component={finalComponent} />;
+// };
