9100cc2e50d489d52f3054739c16adadceb66c4f
Stefan Radusi <stefan.radusi@aeratechnology.com>
2020-12-01 21:42:04 +0200

    landing cover

---
+++ site/src/common/PageCover/PageCover.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import { mergeCssClass } from "../../utils/helpers";
+
+import "./PageCover.css";
+
+export const PageCover = ({ src, backText, frontText, description }) => {
+  const [runAnimation, setRunAnimation] = useState(false);
+
+  useEffect(() => {
+    setRunAnimation(true);
+  }, []);
+
+  return (
+    <div className="page-cover">
+      <img src={src} alt="cover img" className="page-cover__img" />
+      <div className="page-cover__overlay" />
+      <div className="page-cover__title-container">
+        <div
+          className={mergeCssClass(
+            "page-cover__back-text-container",
+            runAnimation && "back-text-container-animation"
+          )}
+        >
+          <p
+            className={mergeCssClass(
+              "page-cover__back-text",
+              runAnimation &&
+                "back-text-animation page-cover__back-text-animation"
+            )}
+          >
+            {backText}
+          </p>
+        </div>
+        <div
+          className={mergeCssClass(
+            "page-cover__front-text-container",
+            runAnimation && "front-text-container-animation"
+          )}
+        >
+          <p
+            className={mergeCssClass(
+              "page-cover__front-text",
+              runAnimation && "front-text-animation"
+            )}
+          >
+            {frontText}
+          </p>
+        </div>
+        <p
+          className={mergeCssClass(
+            "page-cover__description",
+            runAnimation && "description-animation"
+          )}
+        >
+          {description}
+        </p>
+        <div
+          className={mergeCssClass(
+            "page-cover__line",
+            runAnimation && "line-animation"
+          )}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PageCover;
--- site/src/pages/Admin/Admin.js
+++ site/src/pages/Admin/Admin.js
@@ -3,11 +3,13 @@ import { isAdminLogged } from "./AminUtils";
 import { Login } from "./Login";
 import { Tabs } from "./Tabs";
 
+import "./Admin.css";
+
 export const Admin = () => {
   const [userLogged, setLoginStatus] = useState(false);
 
   return (
-    <div className="admin page">
+    <div className="page admin">
       {isAdminLogged(userLogged) ? (
         <Tabs />
       ) : (
