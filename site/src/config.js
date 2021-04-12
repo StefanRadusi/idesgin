5197c7f3ec8912a6509cb864ccd0aa16ba7fd7a0
Stefan Radusi <you@example.com>
2021-04-12 12:29:10 +0300

    change config

--- site/src/config.js
+++ site/src/config.js
@@ -13,6 +13,6 @@ config.domains = {};
  * This will enable your front-end to communicate with your back-end.
  * (e.g. 'https://api.mydomain.com' or 'https://091jafsl10.execute-api.us-east-1.amazonaws.com')
  */
-config.domains.api = "https://8zs42mz7x6.execute-api.us-east-1.amazonaws.com";
+config.domains.api = "https://zjihbeslo1.execute-api.us-east-1.amazonaws.com";
 
 export default config;
